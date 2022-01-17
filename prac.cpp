8e53cc94523cd2cc00184e9493172dde19d868c1
suyan man amatya <srkamatya007@gmail.com>
2022-01-17 13:37:53 +0545

    enter

--- prac.cpp
+++ prac.cpp
@@ -1,26 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int pivot(int arr[], int size)
+class Employee
 {
     int start = 0;
     int end = size - 1;
     int mid = start + (end - start) / 2;
 
-    while (start < end)
-    {
-        if (arr[mid] >= arr[0])
-        {
-            start = mid + 1;
-        }
-        else
-        {
-            end = mid;
-        }
-        mid = start + (end - start) / 2;
-    }
-    return start;
-}
+public:
+    string name;
+    string company;
+    int age;
+
+} deta;
 
 int main()
 {
