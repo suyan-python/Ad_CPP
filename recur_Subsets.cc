ed04dc16283e4f023ed0d9a1627cf2c5de0451fe
suyan man amatya <srkamatya007@gmail.com>
2022-01-30 00:43:24 +0545

    enter

--- recur_Subsets.cc
+++ recur_Subsets.cc
@@ -1,16 +1,51 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-// solve(arr, index + 1, output, ans);
+class Solution
+{
+private:
+    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int> > &ans)
+    {
+        //base case
+        if (index >= nums.size())
+            return;
 
-// int element = arr[index];
-// output.push_back(element);
-// solve(arr, index + 1, output, ans);
+        //exclude
+        solve(nums, output, index + 1, ans);
 
-//input [1,2,3]
-// ->> [],[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]
-// ->> including and excluding game
+        //include
+        int element = nums[index];
+        output.push_back(element);
+        solve(nums, output, index + 1, ans);
+    }
+
+public:
+    vector<vector<int> > subsets(vector<int> &nums)
+    {
+        vector<vector<int> > ans;
+        vector<int> output;
+        int index = 0;
+        solve(nums, output, index, ans);
+        return ans;
+    }
+};
 
 int main()
 {
+    Solution stl;
+    vector<int> nums;
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(3);
+
+    int n = nums.size();
+
+    stl.subsets(nums);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
 }
